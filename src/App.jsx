c65d6bac02afc1db2d97028c35fba9ef099cff3e
Guillaume Amselle Andreux <guillaume@andreux.fr>
2026-01-12 15:02:49 +0100

    feat: ajout de Google Fonts et mise à jour de la structure CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen bg-blue-gray-900 flex items-center justify-center">
+      <div className="text-white">
+        <h1 className="text-4xl font-bold font-roboto mb-4">Weeb</h1>
+        <button className="bg-purple-600 text-white px-6 py-3 rounded-lg font-medium">
+          Test Button
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
